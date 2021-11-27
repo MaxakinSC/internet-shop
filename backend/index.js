639e81ebe1f47fb80ccd1b66421a176f9c91ff43
Pasha <npash2011@gmail.com>
2021-11-27 12:03:38 +0200

    post request

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,28 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 3000
 const mongoose = require('mongoose')
 const carsModel = require('./db/carsModel')
 
-app.get('/:carBrand', async (req, res) => {
+app.use(bodyParser.json());
+
+app.post('/cars', async (req, res) => {
+  const result = await carsModel.create(req.body)
+  res.send(result);
+});
+
+app.get('/cars/:carBrand', async (req, res) => {
   const data1 = await carsModel.find({ brand: req.params.carBrand });
   res.send(data1)
 })
 
-app.get('/:carBrand/:carModel', async (req, res) => {
+app.get('/cars/:carBrand/:carModel', async (req, res) => {
   const data2 = await carsModel.find({ brand: req.params.carBrand, model: req.params.carModel });
   res.send(data2)
 })
 
-app.get('/:carBrand/:carModel/:carYear', async (req, res) => {
+app.get('/cars/:carBrand/:carModel/:carYear', async (req, res) => {
   const data3 = await carsModel.find({ brand: req.params.carBrand, model: req.params.carModel, year: req.params.carYear });
   res.send(data3)
 })
@@ -25,8 +33,8 @@ app.listen(port, () => {
 })
 
 const start = async () => {
-  await mongoose.connect('mongodb://localhost/cars');
+  await mongoose.connect('mongodb://127.0.0.1/cars');
   console.log('mongodb is connected')
   const cars = await carsModel.find();
-  console.log(cars);
+  //console.log(cars);
 }
