d3d6bb5f54cbcd2f73ccecd41df3f139e6f2648d
AlexShchuryshyn <alex0424base@gmail.com>
2021-11-28 19:26:12 +0200

    userspost

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,70 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 3000
 const mongoose = require('mongoose')
 const carsModel = require('./db/carsModel')
+const usersModel = require('./db/usersModel')
 
+app.use(bodyParser.json());
 
-app.get('/:carBrand', async (req, res) => {
+app.post('/cars', async (req, res) => {
+  const result = await carsModel.create(req.body)
+  res.send(result);
+});
+
+app.get('/cars/:carBrand', async (req, res) => {
   const data1 = await carsModel.find({ brand: req.params.carBrand });
   res.send(data1)
 })
 
-app.get('/:carBrand/:carModel', async (req, res) => {
+app.get('/cars/:carBrand/:carModel', async (req, res) => {
   const data2 = await carsModel.find({ brand: req.params.carBrand, model: req.params.carModel });
   res.send(data2)
 })
 
-app.get('/:carBrand/:carModel/:carYear', async (req, res) => {
+app.get('/cars/:carBrand/:carModel/:carYear', async (req, res) => {
   const data3 = await carsModel.find({ brand: req.params.carBrand, model: req.params.carModel, year: req.params.carYear });
   res.send(data3)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-    start();
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+app.post('/users', async (req, res) => {
+  const result1 = await usersModel.create(req.body)
+  res.send(result1);
+});
+
+app.get('/users/:userName', async (req, res) => {
+  const data4 = await usersModel.find({ name: req.params.userName });
+  res.send(data4)
 })
 
+app.get('/users/:usersName/:usersMail', async (req, res) => {
+  const data5 = await usersModel.find({ name: req.params.userName, mail: req.params.userMail });
+  res.send(data5)
+})
+
+app.get('/users/:usersName/:usersMail', async (req, res) => {
+  const data6 = await carsModel.find({ name: req.params.userName, mail: req.params.userMail, year: req.params.userYear });
+  res.send(data6)
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+  start();
+})
+
+
 const start = async () => {
-  await mongoose.connect('mongodb://localhost/cars');
+  await mongoose.connect('mongodb://127.0.0.1/cars');
+
   console.log('mongodb is connected')
   const cars = await carsModel.find();
-  console.log(cars);
+  const users = await usersModel.find();
 }
+
+
+
